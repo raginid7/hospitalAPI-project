7700cd5e4180c1bf3a014cc8c98f73331e8ddfb9
raginid7 <130752255+raginid7@users.noreply.github.com>
2023-12-02 18:46:05 +0530

    book

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+mongoose.connect(process.env.MONGO_URL);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to the db'));
+
+db.once('open', function () {
+  console.log("Successfully connected to the Database");
+});
+
+module.exports = db;
